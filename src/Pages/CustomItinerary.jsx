659798f021238d0b2be9d9952fd8bd459f20daa6
05Pratiksha <05Pratiksha@users.noreply.github.com>
2026-06-26 00:51:21 +0530

    update CustomItinerary

--- src/Pages/CustomItinerary.jsx
+++ src/Pages/CustomItinerary.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 
 export default function CustomItinerary() {
-  const [form, setForm] = useState({
-    name: "",
-    destination: "",
-    days: "",
-    travelers: "",
-    budget: "",
-    activities: "",
-  });
+const [form, setForm] = useState({
+  name: "",
+  contact: "",
+  destination: "",
+  days: "",
+  travelers: "",
+  budget: "",
+  activities: "",
+});
 
   const handleChange = (e) => {
     setForm({
@@ -22,6 +23,7 @@ export default function CustomItinerary() {
 *Custom Travel Itinerary Request*
 
 Name: ${form.name}
+Contact: ${form.contact}
 Destination: ${form.destination}
 Duration: ${form.days} Days
 Travelers: ${form.travelers}
@@ -37,6 +39,16 @@ ${form.activities}
     );
   };
 
+  setForm({
+  name: "",
+  contact: "",
+  destination: "",
+  days: "",
+  travelers: "",
+  budget: "",
+  activities: "",
+});
+
   return (
     <div style={{ maxWidth: "800px", margin: "80px auto", padding: "20px", fontFamily: "'Inter', sans-serif", fontSize: '2.1rem',}}>
       <h1>Create Your Custom Itinerary</h1>
@@ -45,20 +57,23 @@ ${form.activities}
         className="earth-input"
         name="name"
         placeholder="Your Name"
+        value={form.name}
         onChange={handleChange}
       />
 
-       <input
+      <input
         className="earth-input"
-        name="contact number"
-        placeholder="contact number"
+        name="contact"
+        placeholder="Contact Number"
+        value={form.contact}
         onChange={handleChange}
-      />
+    />
       
       <input
         className="earth-input"
         name="destination"
         placeholder="Destination"
+        value={form.destination}
         onChange={handleChange}
       />
 
@@ -66,6 +81,7 @@ ${form.activities}
         className="earth-input"
         name="days"
         placeholder="Number of Days"
+        value={form.days}
         onChange={handleChange}
       />
 
@@ -73,6 +89,7 @@ ${form.activities}
         className="earth-input"
         name="travelers"
         placeholder="Number of Travelers"
+        value={form.travelers}
         onChange={handleChange}
       />
 
@@ -80,6 +97,7 @@ ${form.activities}
         className="earth-input"
         name="budget"
         placeholder="Budget"
+        value={form.budget}
         onChange={handleChange}
       />
 
@@ -88,6 +106,7 @@ ${form.activities}
         rows="6"
         name="activities"
         placeholder="Places, hotels, activities, transport requirements..."
+        value={form.activities}
         onChange={handleChange}
       />
 
